875f0bd5f8913793403cac7ca06118eb1e35171b
Proseken <sunnydaynduke1@gmail.com>
2024-10-09 13:49:05 +0100

    manipulation tasks

---
+++ 0x01-ES6_promise/0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this.maxStudentsSize = maxStudentsSize;
+  }
+
+  set maxStudentsSize(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
