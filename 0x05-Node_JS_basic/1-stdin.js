b8e92407e66a448610bb897b9f3992f5c1b3a738
Proseken <sunnydaynduke1@gmail.com>
2024-11-26 16:31:49 +0100

    create a program named 1-stdin.js that will execute through the command

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// 1. Using Process stdin
+
+const { stdout, stdin } = process;
+
+stdout.write('Welcome to Holberton School, what is your name?\n');
+
+stdin.setEncoding('utf-8');
+stdin.on('readable', () => {
+  stdout.write(`Your name is: ${stdin.read()}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  stdout.write('This important software is now closing\n');
+});
